0dae633b2783884a884560a757b7b4a731ad1621
ebetron <ebetron@gmail.com>
2018-08-12 15:15:04 -0400

    Index via Template

--- DjangoVSBasicProject/HelloDjangoApp/views.py
+++ DjangoVSBasicProject/HelloDjangoApp/views.py
@@ -6,8 +6,12 @@ from datetime import datetime
 def index(request):
     now = datetime.now()
 
-    html_content = "<html><head><title>Hello, Django</title></head><body>"
-    html_content += "<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
-    html_content += "</body></html>"
-
-    return HttpResponse(html_content)
+    return render(
+                  request,
+                  "HelloDjangoApp/index.html", #Relative path from the 'templates' folder to the template file
+                  {
+                      'title' : "Hello Django",
+                      'message' : "Hello Django!",
+                      'content': " on " + now.strftime("%A, %d %B, %Y at %X")
+                  }
+    )
